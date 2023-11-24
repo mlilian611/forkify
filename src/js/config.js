21db89065356c23033703177455a67694c2bbe38
mlilian611 <mdamyanov6@gmail.com>
2023-11-24 18:18:49 +0200

    Inital commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'cd99b413-b583-45b4-b681-4f0f661d8d4c';
+export const MODAL_CLOSE_SEC = 2.5;
